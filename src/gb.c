f5bc6feffa57107ce0859605a681983e8640419a
Felix Briones <f.briones562@gmail.com>
2025-02-06 12:31:10 -0800

    [Load] Implemented LD BC,d16 and LD (BC), A

--- src/gb.c
+++ src/gb.c
@@ -5,32 +5,55 @@
 
 //gb.c: opcodes/registers related to the GameBoy. emu.c: logic related specifically to emulation such as pausing/resuming execution
 
+/*
+ * @brief Handles invalid or unknown opcodes and prints it
+ * @param Pointer to gb struct containing registers
+ * @return void
+ * @note Shouldn't exectute under normal circumstances
+ */
 void invalid(gameBoy_t* gb)
 {
 	printf("Invalid opcode: %#04x\r\n");
 }
 
+/*
+ * @brief Op code function for No Operation instruction (0x00): NOP 
+ * @details Performs no work but consumes 4 clock cycles
+ * @param Pointer to gb struct containing registers
+ * @return void
+ * @note This instruction is 1 byte long and requires 4 cycles to execute
+ */
 void opNOP_0x00(gameBoy_t* gb)
 {
 	printf("opcode: \r\n");
 }
 
-/* 
- * LD BC,d16
- * 3  12
- * - - - -
- * Target: reg BC / Source 16-bit immediate value (next 2 bytes in memory)
- * 3 bytes long: 1 byte for opcode, 2 bytes for immediate value
- * Requires 12 clock cycles to complete
-*/
+/*
+ * @brief Op code function for Load instruction (0x01): LD BC, d16
+ * @details Loads 16-bit immediate value into register BC
+ * @param Pointer to gb struct containing registers
+ * @return void
+ * @note This instruction is 3 bytes long and requires 12 cycles to execute
+ */
 void opLD_0x01(gameBoy_t* gb)
 {
-	printf("LD Executed\r\n");
+	printf("LD 0x01 Executed\r\n"); 
+	uint16_t value = gb->memory[gb->pc + 1] | (gb->memory[gb->pc + 2] << 8);
+	gb->bc = value;
 }
 
+/*
+ * @brief Op code function for Load instruction (0x02): LD (BC), A
+ * @details Loads the value from register A to memory address specified by BC
+ * @param Pointer to gb struct containing registers
+ * @return void
+ * @note This instruction 1 is byte long and requires 8 cycles to execute
+ */
 void opLD_0x02(gameBoy_t* gb)
 {
-	printf("SUB Executed\r\n");
+	printf("LD 0x02 Executed\r\n"); 
+	uint16_t value = gb->a;
+	gb->memory[gb->bc] = value;
 }
 
 /*
@@ -45,6 +68,13 @@ struct gbInstruction gbDispatchTable[GB_NUM_OF_OPCODES] =
 	{ opLD_0x02,    8,       1    },  // LD (BC), A
 };
 
+/*
+ * @brief Function for handling the emulator's dispatch process (fetching, decoding, executing)
+ * @details Uses a function/dispatch table to minimize time complexity by mapping each function pointer's index to their 
+	corresponding op code
+ * @param Pointer to gb struct containing registers
+ * @return void
+ */
 void gbHandleCycle(gameBoy_t* gb)
 {
 	printf("Handling a cycle on the gameBoy");
@@ -54,8 +84,8 @@ void gbHandleCycle(gameBoy_t* gb)
 	{
 		// Execute operation
 		gbDispatchTable[gb->opCode].operation(gb);
-		// Different opcodes have different lengths. Increment PC by length of most recent operation
-		gb->pc+= gbDispatchTable[gb->opCode].opCodeSize * 8;
+		// Different opcodes have different lengths. Increment PC by length of most recent operation in bytes
+		gb->pc+= gbDispatchTable[gb->opCode].opCodeSize;
 		// Set cyclesTarget so we know when to move on
 		gb->cyclesTarget = gb->cyclesCurrent + gbDispatchTable[gb->opCode].clockCycles;
 	}
